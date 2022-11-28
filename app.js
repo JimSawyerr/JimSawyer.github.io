70ae7fc94f530cc2495fc873dea0387ec76a8659
JimSawyerr <119290691+JimSawyerr@users.noreply.github.com>
2022-11-28 16:44:42 +0800

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ b.style.top=j+"px"
 const a = document.querySelector('button.button--yes')
 a.addEventListener("click", Click);
 function Click(){
-   alert('I know mabilis pero im sure what I feel right now\n So im going to take a risk for one last time and I think ito na ang oras nayun hehe\n\n I like you but im scared. ')
-    window.open("webpage.html")
+   alert('Sa Lunch time mo libre ko hehe ')
+    
 
 }
