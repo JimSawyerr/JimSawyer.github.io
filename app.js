93fa7fdb34aace73c27fb88152e504e7bd081f90
JimSawyerr <119290691+JimSawyerr@users.noreply.github.com>
2023-07-11 19:30:30 +0800

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ b.style.top=j+"px"
 const a = document.querySelector('button.button--yes')
 a.addEventListener("click", Click);
 function Click(){
-   alert(" Just take you'r time to heal beshiee. i'll stay here. im glad that i met you cuz of you im happy again wag mo muna ako friend zone okay. im gonna take a risk sayo hehe lets vibe together beshiee <3 ")
+   alert(" I'm not gonna lie \nI will stay by your side\nEven in your lows and highs\nI will not gonna say good bye\n\nYou don't need to ask why\nI want to treat you right\nLet me be your guiding light\nLike a star shining bright")
     
 window.location.href = "beshie.html"
 }
