0f87072a0489ebf1a6350b698f9208a88acf9bad
JimSawyerr <119290691+JimSawyerr@users.noreply.github.com>
2023-07-13 06:13:52 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,117 @@
-const b = document.querySelector('button.button--no')
-b.addEventListener("mouseover", moveHover)
+const b = document.querySelector('button.button--no');
+const a = document.querySelector('button.button--yes');
+const container = document.querySelector('.container');
 
-function moveHover(){
-const i=Math.floor(Math.random()*500)+1 ;
-const j=Math.floor(Math.random()*500)+1 ;
+b.addEventListener("click", clickNo);
+function clickNo() {
+   const position = getRandomPosition();
+   const i = position.x;
+   const j = position.y;
+ 
+   b.style.left = i + "px";
+   b.style.top = j + "px";
+ 
+   displayRandomGIF();
+   updateButtonPosition(i, j);
+ }
+ 
+ function getRandomPosition() {
+   const containerRect = container.getBoundingClientRect();
+   const containerWidth = containerRect.width;
+   const containerHeight = containerRect.height;
+   const buffer = 20; // Buffer zone size in pixels
+ 
+   const randomEdge = Math.floor(Math.random() * 4); // Randomly choose the edge (0, 1, 2, or 3)
+   let x, y;
+ 
+   switch (randomEdge) {
+     case 0: // Top edge
+       x = Math.floor(Math.random() * (containerWidth - buffer * 2)) + buffer;
+       y = buffer;
+       break;
+     case 1: // Right edge
+       x = containerWidth - buffer;
+       y = Math.floor(Math.random() * (containerHeight - buffer * 2)) + buffer;
+       break;
+     case 2: // Bottom edge
+       x = Math.floor(Math.random() * (containerWidth - buffer * 2)) + buffer;
+       y = containerHeight - buffer;
+       break;
+     case 3: // Left edge
+       x = buffer;
+       y = Math.floor(Math.random() * (containerHeight - buffer * 2)) + buffer;
+       break;
+   }
+ 
+   return { x, y };
+ }
+ 
+ 
 
-b.style.left=i+"px"
-b.style.top=j+"px"
+ a.addEventListener("click", clickYes);
 
+function clickYes() {
+  const gifUrl = "https://media.giphy.com/media/l41lOtw1sHjHcgLWo/giphy.gif"; // Replace with the direct URL of the desired Tenor GIF
+  const gifElement = document.createElement("img");
+  gifElement.src = gifUrl;
+  gifElement.classList.add('yes-gif');
+  
+  // Append the GIF element to the body or any desired container
+  document.body.appendChild(gifElement);
 
+  setTimeout(function() {
+    // Remove the GIF element after a certain delay (e.g., 3 seconds)
+    gifElement.remove();
+
+    // Show the alert message
+    alert("I'm not gonna lie\nI will stay by your side\nEven in your lows and highs\nI will not say goodbye\n\nYou don't need to ask why\nI want to treat you right\nLet me be your guiding light\nLike a star shining bright");
+
+    // Navigate to the specified URL
+    window.location.href = "beshie.html";
+  }, 1500); // Adjust the delay as needed (in milliseconds)
 }
-const a = document.querySelector('button.button--yes')
-a.addEventListener("click", Click);
-function Click(){
-   alert("I'm not gonna lie \nI will stay by your side\nEven in your lows and highs\nI will not gonna say good bye\n\nYou don't need to ask why\nI want to treat you right\nLet me be your guiding light\nLike a star shining bright")
-    
-window.location.href = "beshie.html"
-}
+
+ 
+const gifUrls = [
+   "https://media.giphy.com/media/4QxQgWZHbeYwM/giphy.gif",
+   "https://media.giphy.com/media/bSm7ui6ExVru4qamhf/giphy.gif",
+   "https://media.giphy.com/media/ftqLysT45BJMagKFuk/giphy.gif",
+   "https://media.giphy.com/media/wkW0maGDN1eSc/giphy.gif",
+   "https://media.giphy.com/media/QTrG6mjkHEkpFR3DqX/giphy.gif",
+   "https://media3.giphy.com/media/iIYtEOyM8oRkodXUUX/giphy.gif?cid=ecf05e47zaizcgngg757obk3yd14t2kras55gbt554kvhe3h&ep=v1_gifs_related&rid=giphy.gif&ct=g"
+ 
+ ];
+ 
+ let gifIndex = 0;
+ 
+ function displayRandomGIF() {
+   const gifUrl = gifUrls[gifIndex];
+   const gif = document.createElement('img');
+   gif.src = gifUrl;
+   gif.classList.add('random-gif');
+ 
+   gif.onload = function () {
+     const containerRect = container.getBoundingClientRect();
+     const containerWidth = containerRect.width;
+     const containerHeight = containerRect.height;
+ 
+     const randomX = Math.floor(Math.random() * (containerWidth - gif.width));
+     const randomY = Math.floor(Math.random() * (containerHeight - gif.height));
+ 
+     gif.style.left = randomX + "px";
+     gif.style.top = randomY + "px";
+   };
+ 
+   const existingGifs = document.querySelectorAll('.random-gif');
+   existingGifs.forEach(existingGif => existingGif.remove());
+ 
+   container.appendChild(gif);
+ 
+   gifIndex++;
+   if (gifIndex === gifUrls.length) {
+     gifIndex = 0;
+   }
+ }
+ 
+ 
+ 
